77894c151c0119a16f5b6b332b4d48a6fe941e38
LLouis <llouis0622@gmail.com>
2025-07-17 22:55:45 +0900

    .

--- kt-test/app.py
+++ kt-test/app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sys
+from chatbot import Chatbot
+from common import model
 
-app = Flask(__name__)
+chat_bot = Chatbot(model.basic)
+
+application = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@application.route("/")
+def hello():
+    return "Hello goorm!"
 
 
-if __name__ == '__main__':
-    app.run()
+@application.route("/welcome")
+def welcome():  # 함수명은 꼭 welcome일 필요는 없습니다.
+    return "Hello goorm!"
+
+
+@application.route("/chat-app")
+def chat_app():
+    return render_template("index.html")
+
+
+# 메아리 코드
+# @application.route('/chat-api', methods=['POST'])
+# def chat_api():
+#     print("request.json:", request.json)
+#     return {"response_message": "나도 " + request.json['request_message']}
+
+@application.route('/chat-api', methods=['POST'])
+def chat_api():
+    request_message = request.json['request_message']
+    print("request_message:", request_message)
+    chat_bot.add_user_message(request_message)
+    response = chat_bot.send_request()
+    chat_bot.add_response(response)
+    response_message = chat_bot.get_response_content()
+    print("response_message:", response_message)
+    return {"response_message": response_message}
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=int(sys.argv[1]))
--- kt-test/chatbot.py
+++ kt-test/chatbot.py
@@ -1,57 +1,56 @@
-from pprint import pprint
 from common import client, model
-import openai
 
 
 class Chatbot:
-    def __init__(self, api_key):
-        self.context = [
-            {"role": "system", "content": "You are a helpful assistant."}
-        ]
-        self.client = openai.OpenAI(api_key=api_key)
-        self.model = "gpt-4o-mini"
 
-    def add_user_message(self, message):
-        self.context.append({"role": "user", "content": message})
+    def __init__(self, model):
+        self.context = [{"role": "system", "content": "You are a helpful assistant."}]
+        self.model = model
+
+    def add_user_message(self, user_message):
+        self.context.append({"role": "user", "content": user_message})
 
     def send_request(self):
-        self.response = self.client.chat.completions.create(
+        response = client.chat.completions.create(
             model=self.model,
             messages=self.context
-        ).model_dump()
+        )
+        return response.model_dump()
 
-    def add_assistant_response(self):
-        reply = self.response["choices"][0]["message"]
-        self.context.append(reply)
+    def add_response(self, response):
+        self.context.append({
+            "role": response['choices'][0]['message']["role"],
+            "content": response['choices'][0]['message']["content"],
+        }
+        )
 
-    def get_last_response(self):
-        content = self.response["choices"][0]["message"]["content"]
-        print(content)
-        return content
-
-    def print_context(self):
-        print("\n=== Current Chat Context ===")
-        for i, message in enumerate(self.context):
-            role = message["role"]
-            content = message["content"]
-            print(f"{i + 1:02d}. [{role.upper()}] {content}")
-        print("============================\n")
+    def get_response_content(self):
+        return self.context[-1]['content']
 
 
 if __name__ == "__main__":
-    api_key = "-"
-    bot = Chatbot(api_key)
+    # step-3: 테스트 시나리오에 따라 실행 코드 작성 및 예상 출력결과 작성
+    chatbot = Chatbot(model.basic)
 
-    # 1차 질문
-    bot.add_user_message("Who won the world series in 2020?")
-    bot.send_request()
-    bot.add_assistant_response()
-    bot.get_last_response()
-    bot.print_context()
+    chatbot.add_user_message("Who won the world series in 2020?")
 
-    # 2차 질문
-    bot.add_user_message("Where was it played?")
-    bot.send_request()
-    bot.add_assistant_response()
-    bot.get_last_response()
-    bot.print_context()
+    # 시나리오1-4: 현재 context를 openai api 입력값으로 설정하여 전송
+    response = chatbot.send_request()
+
+    # 시나리오1-5: 응답 메시지를 context에 추가
+    chatbot.add_response(response)
+
+    # 시나리오1-7: 응답 메시지 출력
+    print(chatbot.get_response_content())
+
+    # 시나리오2-2: 사용자가 채팅창에 "Where was it played?" 입력
+    chatbot.add_user_message("Where was it played?")
+
+    # 다시 요청 보내기
+    response = chatbot.send_request()
+
+    # 응답 메시지를 context에 추가
+    chatbot.add_response(response)
+
+    # 응답 메시지 출력
+    print(chatbot.get_response_content())
